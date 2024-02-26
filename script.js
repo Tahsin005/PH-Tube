e43377b8280613d1c6789d5808e18237c6850cf6
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-02-26 15:45:26 +0600

    finalized all the sections including categorized and blog section

--- script.js
+++ script.js
@@ -1,9 +1,92 @@
+const loadAllCategory = async () => {
+  const category = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await category.json();
+
+  const tabContainer = document.getElementById("tab-container");
+
+  const allCat = data.data;
+
+  allCat.forEach((cat) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+            <button class="btn btn-active btn-neutral"  onclick="handleCategorizedSection('${cat.category_id}')" class="tab">${cat.category}</button>
+        `;
+    tabContainer.appendChild(div);
+  });
+};
+
+const handleCategorizedSection = async (categoryId) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${categoryId} `
+  );
+  const data = await response.json();
+  const videos = data.data;
+
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  if (videos.length > 0) {
+    videos?.forEach((video) => {
+      const div = document.createElement("div");
+      div.innerHTML = `
+        <div class="card bg-base-100 shadow-xl max-h-80">
+            <figure>
+                <img src=${video?.thumbnail} />
+            </figure>
+        <div class="card-body">
+            <div class="card-footer flex justify-between ">
+                <div class="flex gap-x-8">
+                    <div>
+                        <div class="avatar online">
+                                <div class="w-14 rounded-full">
+                                    <img src=${
+                                      video.authors[0].profile_picture
+                                    } />
+                                </div>
+                        </div>
+                    </div>
+                    <h2 class="card-title"> ${video?.title} </h2>
+                
+                </div>
+                    
+            </div>
+  
+            <div class="flex text-center gap-2">
+                <h6>${video.authors[0].profile_name}</h6>
+                <small>${
+                  video.authors[0].verified
+                    ? `<div>
+                        <img src="./PHero-Tube/verified.png" alt="">
+                    </div>`
+                    : ""
+                }
+                </small>
+            </div>
+        
+            <p>
+                ${video.others.views} views
+            </p>
+            <p>
+            ${convertTime(video.others.posted_date)} 
+            </p>
+      </div>
+    </div>`;
+
+      cardContainer.appendChild(div);
+      document.getElementById("not-found-container").style.display = "block";
+    });
+  } else {
+    document.getElementById("not-found-container").style.display = "block";
+  }
+};
+
 const handleSortByViews = () => {
   fetch(`https://openapi.programming-hero.com/api/videos/category/${"1000"}`)
     .then((response) => response.json())
     .then((data) => {
       const videos = data.data;
-      console.log(videos);
+      //   console.log(videos);
 
       videos.sort((a, b) => {
         const viewA = parseInt(a.others.views);
@@ -11,7 +94,7 @@ const handleSortByViews = () => {
         return viewB - viewA;
       });
 
-      console.log(videos);
+      //   console.log(videos);
 
       const cardContainer = document.getElementById("card-container");
       cardContainer.innerHTML = "";
@@ -29,7 +112,9 @@ const handleSortByViews = () => {
                     <div>
                         <div class="avatar online">
                                 <div class="w-14 rounded-full">
-                                    <img src=${video.authors[0].profile_picture} />
+                                    <img src=${
+                                      video.authors[0].profile_picture
+                                    } />
                                 </div>
                         </div>
                     </div>
@@ -41,10 +126,13 @@ const handleSortByViews = () => {
   
             <div class="flex text-center gap-2">
                 <h6>${video.authors[0].profile_name}</h6>
-                <small>${video.authors[0].verified? 
-                    `<div>
-                        <img src="./fi_10629607.png" alt="">
-                    </div>`: ""}
+                <small>${
+                  video.authors[0].verified
+                    ? `<div>
+                        <img src="./PHero-Tube/verified.png" alt="">
+                    </div>`
+                    : ""
+                }
                 </small>
             </div>
         
@@ -63,35 +151,39 @@ const handleSortByViews = () => {
 };
 
 const convertTime = (time) => {
-	const timeNumber = parseInt(time);
-	let [years, months, days, hours, minutes, seconds] = [0, 0, 0, 0, 0, 0];
-	if (timeNumber > 0) seconds = timeNumber;
-	if (seconds >= 60) {
-		minutes = Math.floor(seconds / 60);
-		seconds %= 60;
-	}
-	if (minutes >= 60) {
-		hours = Math.floor(minutes / 60);
-		minutes %= 60;
-	}
-	if (hours >= 24) {
-		days = Math.floor(hours / 24);
-		hours %= 24;
-	}
-	if (days >= 30) {
-		months = Math.floor(days / 30);
-		days %= 30;
-	}
-	if (months >= 12) {
-		years = Math.floor(months / 12);
-		months %= 12;
-	}
-	const arr = [];
-	if (years) arr.push(`${years} years`);
-	if (months) arr.push(`${months} months`);
-	if (days) arr.push(`${days} days`);
-	if (hours) arr.push(`${hours} hours`);
-	if (minutes) arr.push(`${minutes} minutes`);
-	if (seconds) arr.push(`${seconds} seconds`);
-	return arr.join(" ");
+  const timeNumber = parseInt(time);
+  let [years, months, days, hours, minutes, seconds] = [0, 0, 0, 0, 0, 0];
+  if (timeNumber > 0) seconds = timeNumber;
+  if (seconds >= 60) {
+    minutes = Math.floor(seconds / 60);
+    seconds %= 60;
+  }
+  if (minutes >= 60) {
+    hours = Math.floor(minutes / 60);
+    minutes %= 60;
+  }
+  if (hours >= 24) {
+    days = Math.floor(hours / 24);
+    hours %= 24;
+  }
+  if (days >= 30) {
+    months = Math.floor(days / 30);
+    days %= 30;
+  }
+  if (months >= 12) {
+    years = Math.floor(months / 12);
+    months %= 12;
+  }
+  const TIME = [];
+  if (years) TIME.push(`${years} years`);
+  if (months) TIME.push(`${months} months`);
+  if (days) TIME.push(`${days} days`);
+  if (hours) TIME.push(`${hours} hours`);
+  if (minutes) TIME.push(`${minutes} minutes`);
+  if (seconds) TIME.push(`${seconds} seconds`);
+  return TIME.join(' ');
 };
+
+loadAllCategory();
+
+handleCategorizedSection("1000");
